8d87c36c08a2deb73899f65222bef532f134761a
Oleg <olegbytkov@gmail.com>
2022-01-03 00:47:59 +0300

    First version, 2d

---
+++ main.py
@@ -0,0 +1,23 @@
+import math
+import pygame
+from Settings import *
+from Player import Player
+
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+player = Player()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    player.movement()
+    sc.fill(BLACK)
+
+    pygame.draw.circle(sc, GREEN, player.pos, 12)
+    pygame.draw.line(sc, GREEN, player.pos, (player.x + WIDTH + math.cos(player.angle),
+                                             player.y + WIDTH + math.sin(player.angle)))
+
+    pygame.display.flip()
+    clock.tick(FPS)
